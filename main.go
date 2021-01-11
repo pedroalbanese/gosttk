c88fa57f52ec5ac77b9e1678710cbd52008193f1
Pedro Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-01-11 04:32:21 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-//go:generate goversioninfo -manifest=testdata/resource/goversioninfo.exe.manifest
 package main
 import (
 	"bufio"
@@ -22,14 +21,15 @@ import (
 
 	var pubHex = flag.String("pub", "", "Remote's side public key. (for shared key derivation only)")
 	var crypt = flag.Bool("crypt", false, "Encrypt/Decrypt with Kuznyechik (GOST R 34.12-2015) symmetric cipher.")
-	var derive = flag.Bool("derive", false, "Derive shared key negociation (VKO GOST R 34.10-2012).")
+	var derive = flag.Bool("derive", false, "Derive shared key negociation (VKO).")
 	var mac = flag.Bool("hmac", false, "Compute HMAC-Streebog256/512 (GOST R 34.11-2012).")
 	var key = flag.String("key", "", "Private/Public key, password or HMAC key, depending on operation.")
 	var sig = flag.String("signature", "", "Input signature. (verification only)")
 	var bit = flag.Int("bits", 256, "Bit length: 256 or 512. (digest|generate|sign|VKO)")
+	var mode = flag.Int("mode", 2012, "Mode: 2001 or 2012. (generate|sign|VKO)")
 	var sign = flag.Bool("sign", false, "Sign with private key.")
 	var verify = flag.Bool("verify", false, "Verify with public key.")
-	var generate = flag.Bool("generate", false, "Generate GOST R 34.10-2012 asymmetric keypair.")
+	var generate = flag.Bool("generate", false, "Generate GOST R 34.10-2012 or 34.10-2001 asymmetric keypair.")
 	var digest = flag.Bool("digest", false, "Compute Streebog256/512 (GOST R 34.11-2012) hashsum.")
 	var digest94 = flag.Bool("digest94", false, "Compute GOST94-CryptoPro (GOST R 34.11-94) hashsum.")
 
@@ -46,7 +46,7 @@ func main() {
         os.Exit(1)
         }
 
-
+	
         if *crypt == true {
 	keyHex := key
 	var key []byte
@@ -88,7 +88,7 @@ func main() {
         os.Exit(0)
         }
 
-
+	
         if *mac == true && *bit == 256 {
 	keyHex := key
 	flag.Parse()
@@ -140,9 +140,9 @@ func main() {
         os.Exit(0)
         }
 
-
+	
 	var err error
-        if *derive == true {
+        if *derive == true && *mode == 2012 {
 
 	var curve *gost3410.Curve
 	if *bit == 256 {
@@ -187,8 +187,50 @@ func main() {
 	os.Exit(0)
 	}
 
+	
+        if *derive == true && *mode == 2001 {
 
-	if *generate {
+	var curve *gost3410.Curve
+	curve = gost3410.CurveIdGostR34102001CryptoProAParamSet()
+
+	var prvRaw []byte
+	var pubRaw []byte
+	var prv *gost3410.PrivateKey
+	var pub *gost3410.PublicKey
+
+	prvRaw, err = hex.DecodeString(*key)
+	if err != nil {
+                log.Fatal(err)
+	}
+	if len(prvRaw) != 256/8 {
+                log.Fatal(err, "private key has wrong length")
+	}
+	prv, err = gost3410.NewPrivateKey(curve, prvRaw)
+	if err != nil {
+                log.Fatal(err)
+	}
+	pubRaw, err = hex.DecodeString(*pubHex)
+	if err != nil {
+                log.Fatal(err)
+	}
+	if len(pubRaw) != 2*256/8 {
+                log.Fatal(err, "public key has wrong length")
+	}
+	pub, err = gost3410.NewPublicKey(curve, pubRaw)
+	if err != nil {
+                log.Fatal(err)
+	}
+
+	shared, err := prv.KEK2012256(pub, big.NewInt(1))
+	if err != nil {
+                log.Fatal(err)
+	}
+	fmt.Print("Shared= ", hex.EncodeToString(shared))
+	os.Exit(0)
+	}
+
+	
+	if *generate && *mode == 2012 {
 	var curve *gost3410.Curve
 
 	if *bit == 256 {
@@ -248,6 +290,39 @@ func main() {
 		}
 	}
 
+	
+	if *generate && *mode == 2001 {
+	var curve *gost3410.Curve
+	curve = gost3410.CurveIdGostR34102001CryptoProAParamSet()
+
+	var prvRaw []byte
+	var pubRaw []byte
+	var prv *gost3410.PrivateKey
+	var pub *gost3410.PublicKey
+
+        if *bit == 256 {
+		prvRaw = make([]byte, 256/8)
+		_, err = io.ReadFull(rand.Reader, prvRaw)
+		if err != nil {
+                        log.Fatal(err)
+		}
+		fmt.Println("Private=", hex.EncodeToString(prvRaw))
+
+		prv, err = gost3410.NewPrivateKey(curve, prvRaw)
+		if err != nil {
+                        log.Fatal(err)
+		}
+
+		pub, err = prv.PublicKey()
+		if err != nil {
+                        log.Fatal(err)
+		}
+		pubRaw = pub.Raw()
+		fmt.Println("Public=", hex.EncodeToString(pubRaw))
+
+	}
+	}
+
 
         if *sign == true || *verify == true {
 
@@ -277,7 +352,7 @@ func main() {
                 log.Fatal(err)
 	}
 
-	if *sign == true && *bit == 256 {
+	if *sign == true && *bit == 256 && *mode == 2012 {
 	curve := gost3410.CurveIdtc26gost34102012256paramSetA()
 	prvRaw, err = hex.DecodeString(*key)
 	if err != nil {
@@ -299,7 +374,7 @@ func main() {
 	os.Exit(0)
 	}
 
-	if *verify == true && *bit == 256 {
+	if *verify == true && *bit == 256 && *mode == 2012 {
 	curve := gost3410.CurveIdtc26gost34102012256paramSetA()
 	pubRaw, err = hex.DecodeString(*key)
 	if err != nil {
@@ -322,7 +397,7 @@ func main() {
 	}
 
 
-	if *sign == true && *bit == 512 {
+	if *sign == true && *bit == 512 && *mode == 2012 {
 	curve := gost3410.CurveIdtc26gost341012512paramSetA()
 	prvRaw, err = hex.DecodeString(*key)
 	if err != nil {
@@ -344,7 +419,7 @@ func main() {
 	os.Exit(0)
 	}
 
-	if *verify == true && *bit == 512 {
+	if *verify == true && *bit == 512 && *mode == 2012 {
 	curve := gost3410.CurveIdtc26gost341012512paramSetA()
 	pubRaw, err = hex.DecodeString(*key)
 	if err != nil {
@@ -363,8 +438,53 @@ func main() {
 		}
 	if !isValid { log.Fatal(err, "signature is invalid") }
         fmt.Println("Verify correct.")
+	} 
+
+
+	if *sign == true && *mode == 2001 {
+	var curve *gost3410.Curve
+	curve = gost3410.CurveIdGostR34102001CryptoProAParamSet()
+	prvRaw, err = hex.DecodeString(*key)
+	if err != nil {
+                log.Fatal(err)
 	}
+	if len(prvRaw) != 256/8 {
+                log.Fatal(err, "private key has wrong length")
+	}
+	prv, err = gost3410.NewPrivateKey(curve, prvRaw)
+	if err != nil {
+                log.Fatal(err)
+	}
+
+	signature, err := prv.Sign(rand.Reader, dgst, nil)
+		if err != nil {
+                        log.Fatal(err)
+		}
+		fmt.Print(hex.EncodeToString(signature))
 	os.Exit(0)
 	}
 
+	if *verify == true && *mode == 2001 {
+	var curve *gost3410.Curve
+	curve = gost3410.CurveIdGostR34102001CryptoProAParamSet()
+	pubRaw, err = hex.DecodeString(*key)
+	if err != nil {
+                log.Fatal(err)
+	}
+	if len(pubRaw) != 2*256/8 {
+                log.Fatal(err, "public key has wrong length")
+	}
+	pub, err = gost3410.NewPublicKey(curve, pubRaw)
+	if err != nil {
+                log.Fatal(err)
+	}
+	isValid, err := pub.VerifyDigest(dgst, inputsig)
+		if err != nil {
+                        log.Fatal(err)
+		}
+	if !isValid { log.Fatal(err, "signature is invalid") }
+        fmt.Println("Verify correct.")
+	}
+	os.Exit(0)
+	}
 }
