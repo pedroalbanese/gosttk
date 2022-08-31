950b0c27942f6bc49c1237c04de22c3f095a5b2d
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2022-08-31 15:09:00 -0300

    Update main.go

--- cmd/gosttk/main.go
+++ cmd/gosttk/main.go
@@ -98,7 +98,7 @@ func main() {
 		log.Fatal("RAND must have 64/128/256/512-bit.")
 	}
 
-	if *encode == "e" || *encode == "enc" || *encode == "encode" {
+	if *encode == "enc" || *encode == "encode" {
 		b, err := ioutil.ReadAll(os.Stdin)
 		if len(b) == 0 {
 			os.Exit(0)
@@ -112,7 +112,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	if *encode == "d" || *encode == "dec" || *encode == "decode" {
+	if *encode == "dec" || *encode == "decode" {
 		var err error
 		buf := bytes.NewBuffer(nil)
 		data := os.Stdin
@@ -137,7 +137,18 @@ func main() {
 		os.Exit(0)
 	}
 
-	if *crypt == "enc" && strings.ToUpper(*mode) == "MGM" {
+	if *encode == "dump" {
+		buf := bytes.NewBuffer(nil)
+		data := os.Stdin
+		io.Copy(buf, data)
+		b := strings.TrimSuffix(string(buf.Bytes()), "\r\n")
+		b = strings.TrimSuffix(string(b), "\n")
+		dump := hex.Dump([]byte(b))
+		os.Stdout.Write([]byte(dump))
+		os.Exit(0)
+	}
+
+	if (*crypt == "enc" || *crypt == "dec") && strings.ToUpper(*mode) == "MGM" {
 		var keyHex string
 		var keyRaw []byte
 		if *pbkdf == true && *bit == false {
@@ -203,82 +214,36 @@ func main() {
 		}
 		aead, _ := mgm.NewMGM(c, n)
 
-		nonce := make([]byte, aead.NonceSize(), aead.NonceSize()+len(msg)+aead.Overhead())
+		if *crypt == "enc" {
+			nonce := make([]byte, aead.NonceSize(), aead.NonceSize()+len(msg)+aead.Overhead())
 
-		out := aead.Seal(nonce, nonce, msg, []byte(*info))
-		fmt.Printf("%s", out)
-		os.Exit(0)
-	}
+			if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+				log.Fatal(err)
+			}
 
-	if *crypt == "dec" && strings.ToUpper(*mode) == "MGM" {
-		var keyHex string
-		var keyRaw []byte
-		if *pbkdf == true && *bit == false {
-			keyRaw = pbkdf2.Key([]byte(*key), []byte(*salt), *iter, 32, gost34112012256.New)
-			keyHex = hex.EncodeToString(keyRaw)
-		} else if *pbkdf == true && *bit == true {
-			keyRaw = pbkdf2.Key([]byte(*key), []byte(*salt), *iter, 32, gost34112012512.New)
-			keyHex = hex.EncodeToString(keyRaw)
-		} else {
-			keyHex = *key
+			if *vector != "" {
+				nonce, _ = hex.DecodeString(*vector)
+			}
+
+			nonce[0] &= 0x7F
+
+			out := aead.Seal(nonce, nonce, msg, []byte(*info))
+			fmt.Printf("%s", out)
+
+			os.Exit(0)
 		}
-		var key []byte
-		var err error
-		if keyHex != "" {
-			key, err = hex.DecodeString(keyHex)
+
+		if *crypt == "dec" {
+			nonce, msg := msg[:aead.NonceSize()], msg[aead.NonceSize():]
+
+			out, err := aead.Open(nil, nonce, msg, []byte(*info))
 			if err != nil {
 				log.Fatal(err)
 			}
-			if len(key) != 32 {
-				log.Fatal(err)
-			}
-		} else {
-			os.Exit(1)
-		}
+			fmt.Printf("%s", out)
 
-		buf := bytes.NewBuffer(nil)
-		data := os.Stdin
-		io.Copy(buf, data)
-		msg := buf.Bytes()
-		var c cipher.Block
-		var n int
-		if *block == false && *old == true {
-			if strings.ToUpper(*paramset) == "A" {
-				c = gost28147.NewCipher(key, &gost28147.SboxIdGost2814789CryptoProAParamSet)
-				n = 8
-			} else if strings.ToUpper(*paramset) == "B" {
-				c = gost28147.NewCipher(key, &gost28147.SboxIdGost2814789CryptoProBParamSet)
-				n = 8
-			} else if strings.ToUpper(*paramset) == "C" {
-				c = gost28147.NewCipher(key, &gost28147.SboxIdGost2814789CryptoProCParamSet)
-				n = 8
-			} else if strings.ToUpper(*paramset) == "D" {
-				c = gost28147.NewCipher(key, &gost28147.SboxIdGost2814789CryptoProDParamSet)
-				n = 8
-			} else if strings.ToUpper(*paramset) == "EAC" {
-				c = gost28147.NewCipher(key, &gost28147.SboxEACParamSet)
-				n = 8
-			} else if strings.ToUpper(*paramset) == "Z" {
-				c = gost28147.NewCipher(key, &gost28147.SboxIdtc26gost28147paramZ)
-				n = 8
-			}
-		} else if *block == true && *old == false {
-			c = gost3412128.NewCipher(key)
-			n = 16
-		} else if *block == false && *old == false {
-			c = gost341264.NewCipher(key)
-			n = 8
+			os.Exit(0)
 		}
-		aead, _ := mgm.NewMGM(c, n)
-
-		nonce, msg := msg[:aead.NonceSize()], msg[aead.NonceSize():]
-
-		out, err := aead.Open(nil, nonce, msg, []byte(*info))
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s", out)
-		os.Exit(0)
 	}
 
 	if (*crypt == "enc" || *crypt == "dec") && *block == true && *old == false && (strings.ToUpper(*mode) == "OFB" || strings.ToUpper(*mode) == "CTR" || strings.ToUpper(*mode) == "CFB8") {
